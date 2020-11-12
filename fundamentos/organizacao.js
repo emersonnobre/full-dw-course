3020c4c7367f81f3599a4703fb45b44308ccef08
emersonnobre <egrnobre@gmail.com>
2020-11-12 14:24:42 -0400

    iniciei o repositório

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,11 @@
+// Códigos em JS podem ser organizados em sentenças e blocos
+
+console.log("Imprime algo");
+
+{
+    {
+        {
+            console.log("Imprime outra coisa")
+        }
+    }
+}
